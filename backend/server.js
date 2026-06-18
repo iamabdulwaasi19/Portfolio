b2860c256405ee92f70f95d80c4e93ffa5ee73cd
[Abdulwaasi Saliu] <[wasiu.saliu@miva.edu.ng]>
2026-06-19 00:28:11 +0100

    cors update

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import connectDB from "./config/db.js";
-// import authRoutes from "./routes/auth.js";
-// import projectRoutes from "./routes/projects.js";
-
-// dotenv.config({ path: ".env" });
-
-// connectDB();
-
-// const app = express();
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   }),
-// );
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "Portfolio API is running" });
-// });
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/projects", projectRoutes);
-
-// app.use((err, req, res, next) => {
-//   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-//   res.status(statusCode);
-//   res.json({
-//     message: err.message,
-//     stack: process.env.NODE_ENV === "production" ? null : err.stack,
-//   });
-// });
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, "0.0.0.0", () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 import express from "express";
 import dotenv from "dotenv";
@@ -57,7 +14,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://portfolio-qtww.vercel.app", // ✅ add your Vercel frontend URL
+  "https://portfolio-ruddy-gamma-14.vercel.app/",
 ];
 
 app.use(
